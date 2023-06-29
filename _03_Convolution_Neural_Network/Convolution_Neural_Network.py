af8626cac2a57bac328077c81cb9d1da5ca5082c
LJY2006040205 <131932671+LJY2006040205@users.noreply.github.com>
2023-06-29 19:28:02 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -10,7 +10,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-import torchvision.transforms as transforms
 
 from torch.utils.data import DataLoader
     
@@ -19,7 +18,7 @@ def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
     # 保持本地训练的数据读取和这里一致
     dataset_train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform)
+    dataset_val = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(trainset, batch_size=128,shuffle=True, num_workers=0)
     data_loader_val = DataLoader(testset, batch_size=100,shuffle=False, num_workers=0)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
